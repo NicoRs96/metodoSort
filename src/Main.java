5486c4074fa0204270d7e50f83f4613672b3c9e6
NicoRs96 <56675649+NicoRs96@users.noreply.github.com>
2022-06-28 17:30:49 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+
+        Logger logger = Logger.getLogger(Main.class.getName());
+
+        list.add("luca");
+        list.add("pietro");
+        list.add("alessandro");
+        list.add("vincenzo");
+
+        String curliest = list.toString();
+        logger.info(curliest);
+
+        Collections.sort(list);
+
+        String change = list.toString();
+        logger.info(change);
+    }
+}
